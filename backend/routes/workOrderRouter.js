ccc1dfbe31ff78be0d975ecb294023a5a6c6774f
Francisco <fsanchezbz@gmail.com>
2023-05-17 15:58:02 +0200

    workOrders Control and Route

---
+++ backend/routes/workOrderRouter.js
@@ -0,0 +1,14 @@
+const workRouter = require('express').Router();
+const {
+  getAllWorkOrders,
+  createWorkOrder,
+  updateWorkOrder,
+  deleteWorkOrder,
+} = require('../controllers/workOrderController');
+
+workRouter.get('/all', getAllWorkOrders);
+workRouter.post('/work-orders', createWorkOrder);
+workRouter.patch('/work-orders/:id', updateWorkOrder);
+workRouter.delete('/work-orders/:id', deleteWorkOrder);
+
+module.exports = workRouter;
