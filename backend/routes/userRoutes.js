03fb7f45db6cef1bf859f0c6e47422ea0a414960
Jared Boyd <jboyd115@icloud.com>
2023-05-19 12:07:52 +0200

    removed token checking

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -14,7 +14,7 @@ userRouter.route('/logout').post(logout);
 userRouter.route('/signup').post(createUser);
 userRouter.route('/me').get(checkToken, getOneUser);
 userRouter.route('/all').get(getAllUsers);
-userRouter.route('/update/:id').put(checkToken, updateUser);
+userRouter.route('/update/:id').put(updateUser);
 
 
 module.exports = userRouter;
