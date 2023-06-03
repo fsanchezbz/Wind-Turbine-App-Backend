aa05cb6ea45bb11e65aa6def96b401d3a64894b5
Jared Boyd <jboyd115@icloud.com>
2023-06-03 09:22:23 +0200

    testing cors

--- backend/index.js
+++ backend/index.js
@@ -23,20 +23,13 @@ app.use(cookieParser());
 const corsOptions = {
   origin: [
     'http://localhost:5173',
-    'https://profound-dasik-d1357e.netlify.app',
-    '*'
+    'https://profound-dasik-d1357e.netlify.app'
   ],
   credentials: true,
   optionSuccessStatus: 200
 };
 app.use(cors(corsOptions));
-
-// app.use(function(req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "http://localhost:5173", 'https://profound-dasik-d1357e.netlify.app', '*');
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//   next();
-// });
-
+app.use(cors());
 
 // Routes
 app.use('/users', userRouter);
