78d1426afb9d21a14bee027ee51c52b13914ec6c
Jared Boyd <jboyd115@icloud.com>
2023-06-03 10:31:28 +0200

    Testing cors for mobile

--- backend/index.js
+++ backend/index.js
@@ -46,10 +46,10 @@ app.use(cors(corsOptions));
 // ); 
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173','https://profound-dasik-d1357e.netlify.app'); // Replace with your frontend origin
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Origin', 'http://localhost:5173','https://profound-dasik-d1357e.netlify.app'); // Replace with your frontend origin
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header('Access-Control-Allow-Credentials', 'true');
   next();
 });
 
