9c686805ab88ff992e2b49ec27d01d87cdf6f02f
Jared Boyd <jboyd115@icloud.com>
2023-06-03 09:37:49 +0200

    More testing with cors

--- backend/index.js
+++ backend/index.js
@@ -22,13 +22,28 @@ app.use(express.json());
 app.use(cookieParser());
 
 // CORS configuration
-const corsOptions = {
-  origin: '*',
-  credentials: true,
-  optionSuccessStatus: 200
-};
-app.use(cors(corsOptions));
+// const corsOptions = {
+//   origin: [
+//     'http://localhost:5173',
+//     'https://profound-dasik-d1357e.netlify.app'
+//   ],
+//   credentials: true,
+//   optionSuccessStatus: 200
+// };
+// app.use(cors(corsOptions));
 
+app.use(
+  cors({
+    origin: [
+      'http://localhost:5173',
+      'https://profound-dasik-d1357e.netlify.app'
+      
+    ],
+    credentials: true,
+    optionSuccessStatus:200
+
+  })
+);
 
 // Routes
 app.use('/users', userRouter);
