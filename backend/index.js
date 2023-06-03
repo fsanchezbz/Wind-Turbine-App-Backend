111281d2bbe889ebbc83bfcae1c988b7bcfc2d09
Jared Boyd <jboyd115@icloud.com>
2023-06-03 10:56:44 +0200

    Reverting all back

--- backend/index.js
+++ backend/index.js
@@ -71,11 +71,10 @@ const io = new Server(server, {
 
 // Socket.IO event handling
 io.on('connection', (socket) => {
-  console.log(`User Connected: ${socket.id}`);
 
   socket.on('join_room', (data) => {
     socket.join(data);
-    console.log(`User with ID: ${socket.id} joined room: ${data}`);
+  
   });
 
   socket.on('send_message', (data) => {
