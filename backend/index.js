4b2022e01612bc5039b6210d6e68a2ca58a38975
Jared Boyd <jboyd115@icloud.com>
2023-06-03 10:39:38 +0200

    Testin the cors for mobile

--- backend/index.js
+++ backend/index.js
@@ -25,7 +25,8 @@ app.use(cookieParser());
 const corsOptions = {
   origin: [
     'http://localhost:5173',
-    'https://profound-dasik-d1357e.netlify.app'
+    'https://profound-dasik-d1357e.netlify.app',
+    '*'
   ],
   credentials: true,
   optionSuccessStatus: 200
@@ -46,7 +47,7 @@ app.use(cors(corsOptions));
 // ); 
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:5173','https://profound-dasik-d1357e.netlify.app'); // Replace with your frontend origin
+  res.header('Access-Control-Allow-Origin', 'http://localhost:5173','https://profound-dasik-d1357e.netlify.app', '*'); // Replace with your frontend origin
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   res.header('Access-Control-Allow-Credentials', 'true');
